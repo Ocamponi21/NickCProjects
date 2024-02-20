8e3556659f58da3575f3f0078d57283907336665
Ocamponi21 <nicolasocampo288@gmail.com>
2024-02-20 13:21:07 -0500

    2/20/24

--- App.c
+++ App.c
@@ -1,5 +1,16 @@
 // pre-processor directives 
 
+//in c ++ a namespace is a colection of related functions,
+//classes, objects, and variables 
+
+//the C++ standard library functions, classes, objects, and 
+//variable are in a namespace named std.
+
+//a way to bring in a specific object, function, class, or 
+//variable of the std namespace into an application is with the 
+//help of the using declaration
+
+
 //include needed C and header files 
 #include "Basiclo.c"
 #include "Decisions.c"
@@ -9,7 +20,26 @@
 #include "PassByRef.c"
 
 
-int main(void){
+//int main(void){
+
+    void main(int argc, char* args[]){
+        if (argc == 1) {
+            printf("No command line argument passed other than the program name %s\n", args[0]);
+        }
+
+        if (argc >= 2) {
+            printf("\nNumber of arguments passed: %d", argc);
+            printf("\n---Following are the command line arguments passed---");
+            for (int i = 0; i < argc; i++) {
+                printf("\nargs[%d]: %s", i, args[i]);
+            }
+        }
+
+    //return 0;
+    
+
+//in o
+
     //output();
     //input();
 
@@ -27,7 +57,7 @@ int main(void){
     // loops3();
     // loops4();
     // loops5();
-    // recursion(START);
+    //recursion(START);
 
     /*Functions*/
     //declare 3 character arrays 
@@ -40,7 +70,7 @@ int main(void){
     // printf("%.1f degrees celsius = %.1f degrees fahreheit.\n", 0.0, fahrenheitFromCelsius(0.0));
     // printf("%.1f degrees celsius = %.1f degrees fahreheit.\n", 100.0, fahrenheitFromCelsius(100.0));
     // printAddress();
-    // printf("The global variable i stores its value at memory location %p.\n", getAddress());
+     //printf("The global variable i stores its value at memory location %p.\n", getAddress());
     // printf("The global variable i stores the value %d.\n", getDataAtAddress());
     // printf("The global variable j now stores the value %.2f.\n", storeDataAddress());
     // printf("The size of the value in the global variable j is %d bytes .\n", getDoubleBytes());
@@ -51,11 +81,11 @@ int main(void){
 
     /* Pass By Reference */
     //declare and initilize variables for (x, y) point 
-    double x = 3.0;
-    double y = 4.0;
+    // double x = 3.0;
+    // double y = 4.0;
 
-    //must use the ampersand to use an address of a variable 
-    cartesainToPolar(x, y, &radius, &theta);
-    printf("(%.2f, %.2f) equals (%.2f, %.2f)\n", x, y, radius, theta);
+    // //must use the ampersand to use an address of a variable 
+    // cartesainToPolar(x, y, &radius, &theta);
+    // printf("(%.2f, %.2f) equals (%.2f, %.2f)\n", x, y, radius, theta);
 
 }
